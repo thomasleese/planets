06616637f2baf23f6a91ed0ccb656cc1d83f98dc
Thomas Leese <inbox@thomasleese.me>
2016-10-19 16:10:22 +0100

    Add iOS-MOE files

---
+++ ios-moe/src/me/thomasleese/planets/IOSMoeLauncher.java
@@ -0,0 +1,29 @@
+package me.thomasleese.planets;
+
+import com.badlogic.gdx.backends.iosmoe.IOSApplication;
+import com.badlogic.gdx.backends.iosmoe.IOSApplicationConfiguration;
+import com.intel.moe.natj.general.Pointer;
+import me.thomasleese.planets.Planets;
+
+import ios.foundation.NSAutoreleasePool;
+import ios.uikit.c.UIKit;
+
+public class IOSMoeLauncher extends IOSApplication.Delegate {
+
+    protected IOSMoeLauncher(Pointer peer) {
+        super(peer);
+    }
+
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.useAccelerometer = false;
+        return new IOSApplication(new Planets(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = NSAutoreleasePool.alloc();
+        UIKit.UIApplicationMain(0, null, null, IOSMoeLauncher.class.getName());
+        pool.dealloc();
+    }
+}
